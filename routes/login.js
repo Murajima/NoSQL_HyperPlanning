d1a01bbd92af2e5f097c00f0901991a33a2751d6
Antoine Poussard <antoinepoussard@MacBook-Pro-de-Antoine.local>
2018-05-14 17:21:50 +0200

    Login done

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const session = require('express-session')
+const utils = require('../utils/login.js')
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.render('login/login')
+    res.render('login/login')
+})
+
+router.post('/loginOK', (req, res, next) => {
+    var password = req.body.password
+    var username = req.body.username
+    utils.loginOK(username, password).then((result) => {
+        if (result.etat == 'etudiant') {
+            res.redirect('/register')
+        } else if (result.etat == 'professeur') {
+            res.redirect('/teachers')
+        }
+    }).catch(function(e) {
+      res.redirect('/login') // "zut !"
+    })
+
 })
 
 router.get('/register', (req, res, next) => {
