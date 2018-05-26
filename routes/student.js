1df73b0c542853e86ab016ce2311d68cbd9f7aad
Antoine Poussard <antoine.poussard@gmail.com>
2018-05-26 23:40:35 +0200

    [FIX] bug on asynchronous call and note display

--- routes/student.js
+++ routes/student.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var redis = require('redis')
 var client = redis.createClient('6379', 'redis')
 var Models = require('../models/models.js')
+var student = require('../utils/student.js')
 
 
 router.all('*', (req, res, next) => {
@@ -14,45 +15,8 @@ router.all('*', (req, res, next) => {
 })
 
 router.get('/', function(req, res, next) {
-    var data_list = []
-    Models.Note.find({username: req.session.username}, function (err, note){
-        var notes = note
-        var tmp = {}
-        var key = ''
-        var prev_mat = ''
-        var note_array = []
-        var itemsProcessed = 0
-        var nb_matiere = 0
-        var name = ''
-        client.get(req.session.username, function(err, reply) {
-            var user = reply
-            user = JSON.parse(user)
-            console.log(user)
-            name = user.firstname + ' ' + user.lastname
-        });
-        notes.forEach(function(element) {
-            Models.Matiere.find({_id: element.matiere_id}, function (err, mat){
-                if(mat[0].matiere == prev_mat && note_array != []) {
-                    note_array.push({Note: element.Note, Coef: element.Coef})
-                    data_list[nb_matiere - 1] = {Matiere: key, Notes: note_array}
-                } else {
-                    if (note_array != []){
-                        tmp = []
-                        note_array = []
-                        nb_matiere++
-                    }
-                    key = mat[0].matiere
-                    note_array.push({Note: element.Note, Coef: element.Coef})
-                    prev_mat = key
-                }
-                itemsProcessed ++
-                if (itemsProcessed == notes.length) {
-                    returnList = {'User': name, 'Notes': data_list}
-                    console.log(returnList)
-                    res.render('student/student', { value: returnList})
-                }
-            })
-        })
+    student.getBulletin(req.session.username).then((result) => {
+        res.render('student/student', { value: result})
     })
 })
 
---
+++ utils/student.js
@@ -0,0 +1,69 @@
+var redis = require('redis')
+var client = redis.createClient('6379', 'redis')
+var Models = require('../models/models.js')
+
+function getBulletin(xid) {
+    return new Promise((resolve, reject) => {
+        var data_list = []
+        var list = []
+        var note_array = []
+        var itemsProcessed = 1
+        var key = ''
+        var exists = false
+        Models.Note.find({username: xid}, function (err, notes){
+            var data_list = []
+            client.get(xid, function(err, reply) {
+                var user = reply
+                user = JSON.parse(user)
+                name = user.firstname + ' ' + user.lastname
+            });
+            notes.forEach(function(element) {
+                getNotes(element).then((result) => {
+                    data_list.push(result)
+                    if(notes.length == data_list.length) {
+                        data_list.forEach(function(element){
+                            if(list.length > 0) {
+                                for (var i = 0; i < list.length; i++) {
+                                    if(list[i].Matiere == element.Matiere) {
+                                        note_array = list[i].Notes
+                                        key = list[i].Matiere
+                                        note_array.push({Note: element.Notes[0].Note, Coef: element.Notes[0].Coef})
+                                        list[i] = {Matiere: key, Notes: note_array}
+                                        exists = true
+                                    }
+                                }
+                                if (exists == false) {
+                                    note_array = []
+                                    note_array.push({Note: element.Notes[0].Note, Coef: element.Notes[0].Coef})
+                                    list.push({Matiere: element.Matiere, Notes: note_array})
+                                }
+                            } else {
+                                note_array.push({Note: element.Notes[0].Note, Coef: element.Notes[0].Coef})
+                                list.push({Matiere: element.Matiere, Notes: note_array})
+                            }
+                            if (itemsProcessed == data_list.length) {
+                                var returnList = {'User': name, 'Notes': list}
+                                resolve(returnList)
+                            }
+                            exists = false
+                            itemsProcessed ++
+                        })
+                    }
+                })
+            })
+        })
+    })
+}
+
+function getNotes(element) {
+    var note_array = []
+    return new Promise((resolve, reject) => {
+        Models.Matiere.find({_id: element.matiere_id}, function (err, mat){
+            var key = mat[0].matiere
+            note_array.push({Note: element.Note, Coef: element.Coef})
+            resolve({Matiere: key, Notes: note_array})
+        })
+    })
+}
+
+module.exports = {getBulletin}
